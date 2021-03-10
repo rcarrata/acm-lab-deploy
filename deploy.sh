1ceb260e5ffda206b04e3ed4e3938ae3939b9e6f
rcarrata <rcarrata@redhat.com>
2021-03-10 13:41:43 +0100

    added more deploy

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,14 @@ export SECRETNAME_SEALED="sealedkeys"
 until oc apply -k https://github.com/ocp-tigers/acm-lab-deploy/argocd/install; do sleep 2; done
 sleep 30
 
+ARGOCD_ROUTE=$(oc get route argocd-server -n argocd -o jsonpath='{.spec.host}{"\n"}')
+
+while [ `curl -ks -o /dev/null -w "%{http_code}" https://$ARGOCD_ROUTE` != 200 ];do
+        echo "waiting for ArgoCD"
+        sleep 10
+done
+        echo "ArgoCD operator"
+
 ## Deploy the ACM Lab resources
 oc apply -k acm-lab-deploy/config/overlays/default
 sleep 60
@@ -19,5 +27,25 @@ oc -n "$NAMESPACE" create secret tls "$SECRETNAME" --cert="$PUBLICKEY" --key="$P
 oc -n "$NAMESPACE" label secret "$SECRETNAME" sealedsecrets.bitnami.com/sealed-secrets-key=active
 sleep 10
 
-## Configure the ACM Lab
-oc apply -k acm-lab-config/config/overlays/default
+export AWS_ACCESS_KEY=$(oc get secret aws-creds -n kube-system -o jsonpath='{.data.aws_access_key_id}')
+export AWS_SECRET_KEY=$(oc get secret aws-creds -n kube-system -o=jsonpath='{.data.aws_secret_access_key}')
+export AWS_ACCESS_KEY_ID=$(echo $AWS_ACCESS_KEY | base64 -d)
+export AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_KEY | base64 -d)
+export AWS_DEFAULT_REGION=eu-west-1
+
+export S3_BUCKET="obs-thanos"
+aws s3api create-bucket --bucket $S3_BUCKET --region $AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$AWS_DEFAULT_REGION
+export S3_ENDPOINT="s3.$AWS_DEFAULT_REGION.amazonaws.com"
+envsubst < assets/obs-secret.yaml.tpl > assets/obs-secret.yaml
+kubeseal --cert "${PUBLICKEY_SEALED}" -o yaml --scope cluster-wide < assets/obs-secret.yaml > manifests/rhacm-observability/base/obs-secret.yaml
+git commit -am "added new obs bucket"
+git push
+
+envsubst < assets/cloud-dns-credentials.yaml.tpl > assets/cloud-dns-credentials.yaml
+kubeseal --cert "${PUBLICKEY_SEALED}" -o yaml --scope cluster-wide < assets/cloud-dns-credentials.yaml > manifests/letsencrypt-certs/base/cloud-dns-credentials.yaml
+git commit -am "added new cloud dns credentials"
+git push
+sleep 5
+
+# ACM Deploy the Config for ACM
+oc apply -k https://github.com/ocp-tigers/acm-lab-deploy/acm-lab-config/config/overlays/default
